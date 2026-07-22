2e13876e7afbc93f2a23a171cd3b351e946335ca
JP Antona <jean-paul.antona@sevitahealth.com>
2026-07-22 11:36:43 -0400

    attachment, contentversion, and contentdocument added

---
+++ examples/attachment_create_typed_table.py
@@ -0,0 +1,13 @@
+from sf_utils import get_client
+from sf_utils.db import get_connection, create_table_from_describe
+
+conn = get_connection();
+client = get_client();
+
+create_table_from_describe(
+    table_name="sf_attachment__c",
+    sobject_type="Attachment",
+    fields=["Id", "ParentId", "BodyLength", "ContentType", "CreatedDate", "Owner.Name", "OwnerId", "LastModifiedDate"],
+    client=client,
+    db_conn=conn
+)
